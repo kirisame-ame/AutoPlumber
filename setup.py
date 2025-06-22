7e0d42a87f947487654249248b5e03eef6739ebc
William Andrian Dharma T <williamadt123@gmail.com>
2025-06-22 17:25:37 +0700

    migrate to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='autoplumber',
-    packages=find_packages(include=['autoplumber', 'autoplumber.*']),
-    version='0.1.0',
-    description='AutoPlumber: Automated Data Preprocessing for Machine Learning',
-    author='Kirisame-ame'
-)
